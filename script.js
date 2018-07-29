340db3b08e38c298cc9e1f4b649969d97d987a4b
jeffrey lonergan <jrlone1990@gmail.com>
2018-07-28 21:44:04 -0700

    Question 1 Working

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ GuessingGameQuestions = () => {
     let newNode = document.createElement('h2');
     newNode.appendChild(document.createTextNode(`Welcome ${username}`));
     node.appendChild(newNode);
-}
+};
+
+startQuiz = () => {
+
+  let firstQuestion = prompt("Was I born in Portland, Oregon, Yes or No?");
+  console.log(`first Question answer is ${firstQuestion}`);
+  if(firstQuestion == "yes" || firstQuestion == "yes" || firstQuestion == "y") {
+      alert("Correct, I was born in Portland!")
+      let question1Answer = document.getElementById('question-answers');
+      let newNode = document.createElement('li');
+      newNode.appendChild(document.createTextNode('correct'));
+      question1Answer.appendChild(newNode);
+  }
+  else {
+      alert("I do live in Portland");
+      let question1Answer = document.getElementById("question-answers");
+      let newNode = document.createElement('li');
+      newNode.appendChild(document.createTextNode('Question 1 incorrect'));
+      question1Answer.appendChild(newNode);
+  }
+};
 
 GuessingGameQuestions();
